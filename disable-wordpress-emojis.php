18fb9e6dc3cbd566c71d68ad1f97c410079fa7a7
tquinonero <152458286+tquinonero@users.noreply.github.com>
2026-02-20 03:54:23 +0100

    Create disable-wordpress-emojis.php
    Disable WordPress emoji scripts

---
+++ disable-wordpress-emojis.php
@@ -0,0 +1,4 @@
+<?php
+// Disable WordPress emoji scripts for better performance
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
